641944e5ed4ff90d2020b30faa2c47f109f50964
Haruray <syafiqfaray2@gmail.com>
2023-10-01 14:45:26 +0700

    add conv

---
+++ src/main.m
@@ -0,0 +1,6 @@
+imtool = ImProcTools;
+img = imread('../data/colored/1.png');
+kernel = [0, 1, 0 ; 1,4,1 ;0,1,0];
+conv = imtool.convolution(img, kernel, "fillzero");
+figure;imshow(img);
+figure;imshow(conv);
